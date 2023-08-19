d8bcf070e631f6d827ac5df5aa3ee3a570b5e616
zhafrandev <140816416+zhafrandev@users.noreply.github.com>
2023-08-19 11:28:10 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,49 @@
+// script.js
+
+function fetchData(category) {
+    const dataTable = document.getElementById("data-table");
+    const jsonUrl = `https://zhafrandev.github.io/Proxygo/dataproxy.json?v=${Date.now()}`;
+
+    // Bersihkan tabel sebelum mengisi data baru
+    dataTable.querySelector("tbody").innerHTML = "";
+
+    fetch(jsonUrl)
+        .then(response => response.json())
+        .then(data => {
+            // Memilih properti berdasarkan kategori yang diberikan
+            const selectedData = data[category];
+
+            // Loop through data yang dipilih dan populate the table
+            selectedData.forEach(item => {
+                const newRow = dataTable.querySelector("tbody").insertRow();
+                const ipCell = newRow.insertCell(0);
+                const hostCell = newRow.insertCell(1);
+
+                ipCell.textContent = item.IP;
+                hostCell.textContent = item.HOST;
+            });
+        })
+        .catch(error => {
+            console.error("Error fetching JSON:", error);
+        });
+}
+
+// Memanggil data "united_states" saat halaman dimuat pertama kali
+document.addEventListener("DOMContentLoaded", function () {
+    fetchData("united_states");
+});
+
+// Memanggil data "united_states" saat halaman dimuat pertama kali
+document.addEventListener("DOMContentLoaded", function () {
+    fetchData("canada");
+});
+
+// Tambahkan event listener ke tombol "Segarkan Data United States"
+document.getElementById("refresh-button-us").addEventListener("click", function () {
+    fetchData("united_states");
+});
+
+// Tambahkan event listener ke tombol "Segarkan Data Canada"
+document.getElementById("refresh-button-ca").addEventListener("click", function () {
+    fetchData("canada");
+});
