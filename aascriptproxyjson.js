7326564c5fb4cd84c476fb36665375f91a4b8bfb
zhafrandev <140816416+zhafrandev@users.noreply.github.com>
2023-10-20 10:34:51 +0700

    Update aascriptproxyjson.js

--- aascriptproxyjson.js
+++ aascriptproxyjson.js
@@ -2,7 +2,7 @@
 
 function fetchData(category) {
     const dataTable = document.getElementById("data-table");
-    const jsonUrl = `https://zhafrandev.github.io/Proxygo/dataproxyjson.json?v=${Date.now()}`;
+    const jsonUrl = `https://zhafrandev.github.io/Proxygo/aadataproxyjson.json?v=${Date.now()}`;
 
     // Bersihkan tabel sebelum mengisi data baru
     dataTable.querySelector("tbody").innerHTML = "";
